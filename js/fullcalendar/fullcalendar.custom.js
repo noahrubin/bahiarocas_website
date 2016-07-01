7fe79838d12d1cf6055629190f84c801e3b8c1a2
noahrubin <noahski@benrubin.net>
2016-07-01 10:51:48 -0500

    pushing fullcalendar

---
+++ js/fullcalendar/fullcalendar.custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#calendar").fullCalendar({
+        googleCalendarApiKey: "AIzaSyD-mJjjbQ54nLu24mwBss6u5tRXm5Q1vKA",
+        events: {
+            googleCalendarId: "rnkardfkjdcc1h5mi7oqq9rip0@group.calendar.google.com"
+        }
+    });
+});
