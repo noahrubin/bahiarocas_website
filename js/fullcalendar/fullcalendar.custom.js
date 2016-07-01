540b81f2bbc4d4446050ac95ebfbf6d45d89de53
noahrubin <noahski@benrubin.net>
2016-07-01 11:18:05 -0500

    fixed calendar showing names issues

--- js/fullcalendar/fullcalendar.custom.js
+++ js/fullcalendar/fullcalendar.custom.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
     $("#calendar").fullCalendar({
         googleCalendarApiKey: "AIzaSyD-mJjjbQ54nLu24mwBss6u5tRXm5Q1vKA",
         events: {
-            googleCalendarId: "rnkardfkjdcc1h5mi7oqq9rip0@group.calendar.google.com"
+            googleCalendarId: "rnkardfkjdcc1h5mi7oqq9rip0@group.calendar.google.com",
+            eventDataTransform: function(eventData) {
+                if (eventData.title) {
+                    eventData.title = eventData.title.slice(0,eventData.title.indexOf("-")-1)
+                    return eventData;
+                }
+            }
         }
     });
 });
