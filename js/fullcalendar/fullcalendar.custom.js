6b173a1facc0aaaefb4379a3b8993709b0e990be
noahrubin <noahski@benrubin.net>
2016-07-01 21:23:29 -0500

    added handler to remove data from calendar

--- js/fullcalendar/fullcalendar.custom.js
+++ js/fullcalendar/fullcalendar.custom.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
         googleCalendarApiKey: "AIzaSyD-mJjjbQ54nLu24mwBss6u5tRXm5Q1vKA",
         events: {
             googleCalendarId: "rnkardfkjdcc1h5mi7oqq9rip0@group.calendar.google.com",
+            // eventDataTransform mutates incoming event data
+            // removes names from title of events (depends on format 'reserved - name_here')
             eventDataTransform: function(eventData) {
                 if (eventData.title) {
                     eventData.title = eventData.title.slice(0,eventData.title.indexOf("-")-1)
